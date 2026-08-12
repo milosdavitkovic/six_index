fe8248755ed48c1cefa5b6cbf3c09dc3403583b8
e559706 <milos.davitkovic@sbb.ch>
2026-08-12 11:35:40 +0200

    Upgrade to Spring Boot 4.1 and CI actions
    Bump the project to Spring Boot 4.1.0 and update the application/test imports and test dependencies for the new package layout and mocking/webmvc test APIs. CI workflows are also refreshed to newer GitHub Actions versions so build and packaging continue to run against the upgraded stack.

--- src/test/java/com/six/indexreview/application/CsvImportServiceTest.java
+++ src/test/java/com/six/indexreview/application/CsvImportServiceTest.java
@@ -22,7 +22,7 @@ import com.six.indexreview.infrastructure.persistence.repository.SpiUniverseRepo
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.mock.web.MockMultipartFile;
 import org.mockito.ArgumentCaptor;
 
@@ -44,28 +44,28 @@ class CsvImportServiceTest {
     @Autowired
     private CsvImportService csvImportService;
 
-    @MockBean
+    @MockitoBean
     private SpiUniverseCsvReader spiUniverseCsvReader;
 
-    @MockBean
+    @MockitoBean
     private SecurityDataCsvReader securityDataCsvReader;
 
-    @MockBean
+    @MockitoBean
     private CompositionCsvReader compositionCsvReader;
 
-    @MockBean
+    @MockitoBean
     private SecurityRepository securityRepository;
 
-    @MockBean
+    @MockitoBean
     private SpiUniverseRepository spiUniverseRepository;
 
-    @MockBean
+    @MockitoBean
     private MarketDataRepository marketDataRepository;
 
-    @MockBean
+    @MockitoBean
     private IndexCompositionRepository indexCompositionRepository;
 
-    @MockBean
+    @MockitoBean
     private IndexDefinitionProvider indexDefinitionProvider;
 
     @Test
